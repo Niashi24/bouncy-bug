a2bc606d122fdb8c1c7f206e09240f36e136d812
Niashi <niasparacino4@gmail.com>
2025-02-04 19:22:00 +0100

    now runs on device :)

--- src/lib.rs
+++ src/lib.rs
@@ -2,81 +2,19 @@
 #![feature(debug_closure_helpers)]
 #![no_std]
 
-mod game;
-
 extern crate alloc;
 #[macro_use]
 extern crate playdate as pd;
+mod game;
 
-use core::cell::OnceCell;
-use core::ptr::NonNull;
 use bevy_app::App;
-use bevy_playdate::DefaultPlugins;
+use core::ptr::NonNull;
 use pd::display::Display;
-use pd::sys::EventLoopCtrl;
-use pd::sys::ffi::{LCDColor, PDSystemEvent, PlaydateAPI};
-use bevy_playdate::event::SystemEvent;
 use pd::graphics::bitmap::LCDColorConst;
-use pd::graphics::draw_line;
-use pd::sys::log::println;
+use pd::sys::ffi::{PDSystemEvent, PlaydateAPI};
+use pd::sys::EventLoopCtrl;
 use pd::system;
-use pd::system::System;
 use pd::system::update::{Update, UpdateCtrl};
-
-/// Game state
-struct State {
-    app: App,
-}
-
-impl State {
-    fn new() -> Self {
-        let mut app = App::new();
-        // Set FPS to 30
-        Display::Default().set_refresh_rate(50.0);
-        
-        // app.add_plugins(DefaultPlugins)
-        //     .add_plugins(game::GamePlugin);
-
-        Self { app }
-        // Self {}
-    }
-
-    /// System event handler
-    fn event(&'static mut self, event: SystemEvent) -> EventLoopCtrl {
-        match event {
-            // Initial setup
-            // SystemEvent::Init => {
-            // 
-            //     // Register our update handler that defined below
-            //     // self.set_update_handler();
-            //     
-            // 
-            //     // println!("Game init complete");
-            // }
-            // TODO: React to other events
-            e => {
-                // println!("test {:?}", event);
-                // self.app.world_mut().trigger(e);
-            }
-        }
-        EventLoopCtrl::Continue
-    }
-}
-
-impl Update for State {
-    /// Updates the state
-    fn update(&mut self) -> UpdateCtrl {
-        // clear(Color::WHITE);
-
-        // self.app.update();
-        // self.app.run_system
-        // self.app.update();
-        draw_line(0, 0, 100, 200, 10, LCDColor::BLACK);
-
-        UpdateCtrl::Continue
-    }
-}
-
 
 /// Entry point
 #[no_mangle]
@@ -96,7 +34,6 @@ fn event_handler(api: NonNull<PlaydateAPI>, event: PDSystemEvent, _: u32) -> Eve
     // unsafe { Instant::set_elapsed(bevydate_time::init(api)) };
 
     let mut app = App::new();
-    
 
     Display::Default().set_refresh_rate(50.);
 
@@ -118,40 +55,6 @@ fn event_handler(api: NonNull<PlaydateAPI>, event: PDSystemEvent, _: u32) -> Eve
 
     EventLoopCtrl::Continue
 }
-// pub fn event_handler(
-//     _api: NonNull<PlaydateAPI>,
-//     event: PDSystemEvent,
-//     sim_key_code: u32,
-// ) -> EventLoopCtrl {
-//     // Unsafe static storage for our state.
-//     // Usually it's safe because there's only one thread.
-//     let event = SystemEvent::from_event(event, sim_key_code);
-//     
-//     if event == SystemEvent::Init {
-//         let mut state = State::new();
-//         
-//         System::Default().set_update_callback_boxed(
-//             move |_| {
-//                 state.update();
-//                 
-//                 UpdateCtrl::Continue
-//             },
-//             ()
-//         )
-//     }
-//     
-//     EventLoopCtrl::Continue
-// 
-//     // pub static mut STATE: OnceCell<State> = OnceCell::new();
-//     
-//     
-//     // App::new();
-//     // System::Cached().set_update_callback_boxed()
-// 
-//     // Call state.event
-//     // #[allow(static_mut_refs)]
-//     // unsafe { STATE.get_mut_or_init(State::new).event(event) }
-// }
 
 
 // Needed for debug build, absolutely optional
