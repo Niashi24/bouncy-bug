5742d5f9f24f3d694009b4762946ae2b5b18728b
Niashi <niasparacino4@gmail.com>
2025-02-05 16:03:15 +0100

    rev, not commit

--- src/game.rs
+++ src/game.rs
@@ -1,24 +1,21 @@
 ﻿use bevy_app::{App, Plugin, Startup, Update};
 use bevy_ecs::system::{Commands, Res};
-use bevy_playdate::DefaultPlugins;
 use bevy_playdate::input::CrankInput;
-use bevy_playdate::sprite::Sprite;
-use pd::graphics::{draw_ellipse, draw_line, Graphics};
 use pd::graphics::color::LCDColorConst;
+use pd::graphics::{draw_ellipse, draw_line};
 use pd::sys::ffi::LCDColor;
-use pd::sys::log::println;
 
 pub struct GamePlugin;
 
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(DefaultPlugins);
         app.add_systems(Startup, draw_test);
         app.add_systems(Update, crank_test);
     }
 }
 
 fn draw_test(mut commands: Commands) {
+    // commands.
     // commands.spawn(Sprite::new());
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -6,12 +6,15 @@ extern crate playdate as pd;
 mod game;
 
 use bevy_app::App;
+use bevy_playdate::DefaultPlugins;
 use crate::game::GamePlugin;
 
 #[bevy_playdate::init_app]
 fn init_app() -> App {
     let mut app = App::new();
-    app.add_plugins(GamePlugin);
+    app
+        .add_plugins(GamePlugin)
+        .add_plugins(DefaultPlugins);
     
     app
 }
