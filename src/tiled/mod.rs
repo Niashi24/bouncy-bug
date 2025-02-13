b546e8cce2a821c91e759b64027c7b11e452df09
Niashi <niasparacino4@gmail.com>
2025-02-13 17:18:26 +0100

    Add export of type registry to tiled

--- src/tiled/mod.rs
+++ src/tiled/mod.rs
@@ -1,17 +1,36 @@
 ﻿use alloc::string::ToString;
-use bevy_app::{App, Plugin};
-use bevy_ecs::system::NonSendMut;
+use core::ops::Deref;
+use bevy_app::{App, Plugin, Startup};
+use bevy_ecs::reflect::AppTypeRegistry;
+use bevy_ecs::system::{NonSendMut, Res};
+use no_std_io2::io::Write;
 use portable_atomic_util::Arc;
 use tiled::{DefaultResourceCache, Loader, ResourceCache, ResourcePath, Template, Tileset};
 use bevy_playdate::asset::AssetCache;
+use bevy_playdate::file::{BufferedWriter, FileHandle};
 
 mod io;
 pub mod loader;
+mod export;
+mod types_json;
+mod load;
 
 pub struct TiledPlugin;
 
 impl Plugin for TiledPlugin {
     fn build(&self, app: &mut App) {
-        
+        app.add_systems(Startup, export_types);
     }
 }
+
+fn export_types(reg: Res<AppTypeRegistry>) {
+    let path = "type-export.json";
+    let file = FileHandle::write_only(path, false).unwrap();
+    let mut writer = BufferedWriter::new_default(file);
+    let registry = export::TypeExportRegistry::from_registry(reg.0.read().deref());
+    let output = serde_json::to_vec_pretty(&registry.to_vec()).unwrap();
+    writer.write_all(&output).unwrap();
+    
+    println!("exported types to {path}");
+}
+
---
+++ src/tiled/types_json.rs
@@ -0,0 +1,80 @@
+﻿use alloc::string::String;
+use alloc::vec::Vec;
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Eq, PartialEq, Hash, Debug, Serialize, Deserialize)]
+pub(crate) struct TypeExport {
+    pub id: u32,
+    pub name: String,
+    #[serde(flatten)]
+    pub type_data: TypeData,
+}
+
+#[derive(Clone, Eq, PartialEq, Hash, Debug, Serialize, Deserialize)]
+#[serde(tag = "type", rename_all = "camelCase")]
+pub(crate) enum TypeData {
+    Enum(Enum),
+    Class(Class),
+}
+
+#[derive(Clone, Eq, PartialEq, Hash, Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub(crate) struct Class {
+    pub use_as: Vec<UseAs>,
+    pub color: String,
+    pub draw_fill: bool,
+    pub members: Vec<Member>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub(crate) struct Member {
+    pub name: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub property_type: Option<String>,
+    #[serde(rename = "type")]
+    pub type_field: FieldType,
+    pub value: serde_json::Value,
+}
+
+#[derive(Clone, Eq, PartialEq, Hash, Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub(crate) struct Enum {
+    pub storage_type: StorageType,
+    pub values: Vec<String>,
+    pub values_as_flags: bool,
+}
+
+#[derive(Copy, Clone, Eq, PartialEq, Hash, Debug, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub(crate) enum StorageType {
+    String,
+    Int,
+}
+
+#[derive(Copy, Clone, Eq, PartialEq, Hash, Debug, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub(crate) enum FieldType {
+    Bool,
+    Color,
+    Float,
+    File,
+    Int,
+    Object,
+    String,
+    Class,
+}
+
+#[derive(Copy, Clone, Eq, PartialEq, Hash, Debug, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub(crate) enum UseAs {
+    Property,
+    Map,
+    Layer,
+    Object,
+    Tile,
+    Tileset,
+    WangColor,
+    WangSet,
+    Project,
+}
